9c396c52742b888d26cb2e82ee989035339fea30
claurier <carl.laurier@sword-group.com>
2018-07-18 11:16:03 +0200

    fonction de méthode de split d'un tableau

--- src/components/home/home-component.ts
+++ src/components/home/home-component.ts
@@ -9,10 +9,10 @@ import 'jstree';
 import {WebAPI} from '../../web-api';
 
 
-@inject(HttpClient, WebAPI, EventAggregator )
+@inject(HttpClient, WebAPI, EventAggregator)
 export class HomeComponent {
   views = ['Agrégée', 'Pièce', 'Qualité'];
-  @bindable currentView = "Agrégée";
+  @bindable currentView = 'Agrégée';
   @bindable config = {
     pageSize: 5,
     totalItems: 0
@@ -28,9 +28,13 @@ export class HomeComponent {
 
   // cette méthode est en fait exécutée
   created() {
-    console.log("methode created dans classe Home Component exécutée ")
+    console.log('methode created dans classe Home Component exécutée ')
     this.api.getContactList().then(contacts => this.contacts = contacts);
-    this.api.getContactListCount().then(total => this.config.totalItems = total);
+    this.api.getContactListCount()
+      .then(total => {
+        this.config.totalItems = total ;
+        this.loadAchatsStats();
+      });
   }
 
   setPage(num) {
@@ -41,28 +45,34 @@ export class HomeComponent {
   /* ********************************************** Tableau Agrégé/Qualité ********************************************** */
   /* ******************************************************************************************************************** */
 
-/*  loadAchatsStats() {
+  loadAchatsStats() {
     this.achatsStats = [];
     this.achatsStatsReady = false;
-
-  };*/
+    var i,  chunk = 5;
+    var temparray = this.contacts;
+    var j = this.config.totalItems;
+    for (i = 0; i < j; i += chunk) {
+      temparray = this.contacts.slice(i, i + chunk);
+      console.log(temparray);
+    }
+  };
 
   currentPageChanged(newValue, oldValue) {
     //if (newValue != oldValue) this.loadAchatsStats()
   };
 
   /*loadUserProfile() {
-    this.http.fetch('/v1/resources/login', {
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      method: 'get'
-    })
-      .then(response => response.json())
-      .then(data => {
-        this.currentPage = 1;
-        this.loadAchatsStats();
-      })
-  };*/
+   this.http.fetch('/v1/resources/login', {
+   headers: {
+   'Content-Type': 'application/json'
+   },
+   method: 'get'
+   })
+   .then(response => response.json())
+   .then(data => {
+   this.currentPage = 1;
+   this.loadAchatsStats();
+   })
+   };*/
 
 }
